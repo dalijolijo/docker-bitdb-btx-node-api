67a2eadf6a4dc1b7328060136bd9ebc6713836b6
David <37987466+dalijolijo@users.noreply.github.com>
2019-04-10 00:39:46 +0200

    Add Grafana docker container

--- install_docker.sh
+++ install_docker.sh
@@ -21,7 +21,7 @@ sudo apt-get install docker-ce
 
 # Install docker-compose
 # https://docs.docker.com/compose/install/#install-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Installed Docker"
